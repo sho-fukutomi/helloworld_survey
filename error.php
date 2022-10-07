e7ea5d07e6797dd9784e5407601fee7ce1cc7d90
sho.fukutomi <hnut.fukutomi@gmail.com>
2022-10-07 13:26:06 +0800

    add discription to error page

--- error.php
+++ error.php
@@ -7,8 +7,14 @@
 	</head>
 	<body>
 		<div class="container">
-			<div>エラーが発生しました</div>
-			<div>URLをご確認の上、管理者にご連絡ください。</div>
+			<h2>ご入力頂いた注文番号ではアンケートに回答出来ません。</h2>
+			<div class="items">以下をご確認頂き、解決しない場合はLINEにてお問合せ下さい。</div>
+			<div class="items">ご購入後7日以上しないとアンケートへご回答頂けません。</div>
+			<div class="items">
+				<div>注文番号が正しい事をご確認下さい。</div>
+				<div>・‐(ハイフン)もご入力下さい。</div>
+				<div>・スペース等が入らないようにご留意ください。</div>
+			</div>
 		</div>
 	</body>
 </html>
